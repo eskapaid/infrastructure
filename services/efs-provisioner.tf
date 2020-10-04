3e657b5bc3367259ca6cdea2a6a278ef14b39f43
Simon KP <simon@eskapa.id>
2020-10-04 16:35:04 +1100

    Add EFS Provisioner repo

--- services/efs-provisioner.tf
+++ services/efs-provisioner.tf
@@ -1,7 +1,9 @@
 resource "helm_release" "efs_provisioner" {
-  name      = "efs-provisioner"
-  chart     = "efs-provisioner"
-  namespace = "default"
+  name       = "efs-provisioner"
+  chart      = "stable/efs-provisioner"
+  repository = "https://kubernetes-charts.storage.googleapis.com"
+  version    = "0.13.0"
+  namespace  = "default"
 
   values = [
     templatefile("${path.module}/values/efs-provisioner.yaml", {
