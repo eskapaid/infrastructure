3ffc8a6f5371a3cbc6492166cd4f0e5dd67c829d
Simon KP <simon@eskapa.id>
2020-10-04 17:25:45 +1100

    EFS mount target per AZ

--- efs.tf
+++ efs.tf
@@ -6,8 +6,20 @@ resource "aws_efs_file_system" "rocketpool" {
   }
 }
 
-resource "aws_efs_mount_target" "rocketpool" {
+resource "aws_efs_mount_target" "rocketpool_a" {
   file_system_id  = aws_efs_file_system.rocketpool.id
   subnet_id       = module.vpc.private_subnets.0
+  security_groups = [aws_security_group.efs.id]
+}
+
+resource "aws_efs_mount_target" "rocketpool_b" {
+  file_system_id  = aws_efs_file_system.rocketpool.id
+  subnet_id       = module.vpc.private_subnets.1
+  security_groups = [aws_security_group.efs.id]
+}
+
+resource "aws_efs_mount_target" "rocketpool_c" {
+  file_system_id  = aws_efs_file_system.rocketpool.id
+  subnet_id       = module.vpc.private_subnets.2
   security_groups = [aws_security_group.efs.id]
 }
