fa6d06d93a5162c1fb212b5ab8c997985cf777e8
Simon KP <simon@techops.services>
2022-09-15 10:23:57 +1000

    formatlist

--- data.tf
+++ data.tf
@@ -14,7 +14,7 @@ data "aws_caller_identity" "current" {}
 
 ## data for erigon1
 data "template_file" "userdata_erigon1" {
-  template = formatlist("%s!",
+  template = formatlist("%s",
   [file("templates/common/userdata.sh")])
   # file("templates/erigon/userdata.sh"),
   # file("templates/lighthouse/userdata.sh"))
